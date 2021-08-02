6c2694aaa8a2b6dad7304b3f3d78b297ce203299
zhoushiji0101 <1457722857@qq.com>
2021-08-02 14:44:38 +0800

    数据库辅助

---
+++ DAL/DBHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace DAL
+{
+    public class DBHelper
+    {
+        //静态的数据库连接字符串
+        public static string connStr = "Data Source=.;Initial Catalog=Attendance;Integrated Security=True";
+        //公共的链接对象
+        public static SqlConnection conn = new SqlConnection(connStr);
+        //如果conn的状态是关闭的，打开连接
+        public static void Open()
+        {
+            if (conn != null && conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+        //如果conn的状态是打开的，关闭连接
+        public static void Closed()
+        {
+            if (conn != null && conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+
+        //公共的查询方法
+        public static DataTable comSelect(string sql)
+        {
+            //命令对象，检查sql语句和连接对象
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            //if (sp != null)
+            //{
+            //    cmd.Parameters.AddRange(sp);
+            //}
+            //适配器，检查sql语句和连接对象
+            SqlDataAdapter sda = new SqlDataAdapter(sql, conn);
+            //定义数据表格
+            DataTable dt = new DataTable();
+            //把表格填充到适配器
+            sda.Fill(dt);
+            return dt;//返回表格
+        }
+
+
+        //增删改公用方法
+        public static int insertUpDel(string sql)
+        {
+            try
+            {
+                Open();//打开连接
+                //命令对象，检查sql语句和连接对象
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //if (sp!=null)
+                //{
+                //    cmd.Parameters.AddRange(sp);
+                //}
+                //执行sql语句，并返回影响记录行数
+                return  cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally {
+                Closed();//最后关闭连接
+            }            
+        }
+
+
+
+
+
+
+    }
+}
--- DAL/LoginDAL.cs
+++ DAL/LoginDAL.cs
@@ -10,19 +10,20 @@ namespace DAL
 {
     public class LoginDAL
     {
-        const string connString = "Data Source=.;Initial Catalog=Attendance;Integrated Security=True";
-        //创建连接对象
-        static SqlConnection conn = new SqlConnection(connString);
+        //const string connString = "Data Source=.;Initial Catalog=Attendance;Integrated Security=True";
+        ////创建连接对象
+        //static SqlConnection conn = new SqlConnection(connString);
 
         public int login(string username, string userpwd)
         {
-            //实例化数据集对象
-            DataTable dt = new DataTable();
+            ////实例化数据集对象
+            //DataTable dt = new DataTable();
             //创建T-SQL语句
             string sql = string.Format("select * from UserInfo where UserName='{0}' and Password='{1}'", username, userpwd);
             //创建数据适配器对象
-            SqlDataAdapter sda = new SqlDataAdapter(sql, conn);
-            sda.Fill(dt);
+            //SqlDataAdapter sda = new SqlDataAdapter(sql, conn);
+            //sda.Fill(dt);
+            DataTable dt=DBHelper.comSelect(sql);
             return dt.Rows.Count;
         }
     }
